ea600d67412d60265807b48459f619957cf578c5
Greg Corbett <greg.corbett@stfc.ac.uk>
2018-05-03 10:30:58 +0100

    Move draw/blit calls into the game loop
    - so the locations things are drawn at can be programatically
      chnaged as part of the game loop

--- PythonLessons/PyGame/pygame-lesson1.py
+++ PythonLessons/PyGame/pygame-lesson1.py
@@ -10,27 +10,22 @@ WINDOWWIDTH = 400
 WINDOWHEIGHT = 300
 
 windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
-windowSurface.fill(BLACK)
-
 basicFont = pygame.font.SysFont(None, 48)
-
 text = basicFont.render('Hello world!', True, WHITE, BLACK)
-textRect = text.get_rect()
-
-textRect.centerx = windowSurface.get_rect().centerx
-textRect.centery = windowSurface.get_rect().centery
-
-# draw the text onto the surface
-windowSurface.blit(text, textRect)
-
-
-pygame.draw.circle(windowSurface, (255,0,0), (10, 20), 30)
-pygame.draw.line(windowSurface, (0,0,255), (200, 250), (225, 300))
-pygame.draw.rect(windowSurface, (0,255,0), (150,50,100,50))
-
-
 
 while True:
+    windowSurface.fill(BLACK)
+
+    textRect = text.get_rect()
+    textRect.centerx = windowSurface.get_rect().centerx
+    textRect.centery = windowSurface.get_rect().centery
+    # draw the text onto the surface
+    windowSurface.blit(text, textRect)
+
+    pygame.draw.circle(windowSurface, (255,0,0), (10, 20), 30)
+    pygame.draw.line(windowSurface, (0,0,255), (200, 250), (225, 300))
+    pygame.draw.rect(windowSurface, (0,255,0), (150,50,100,50))
+
     pygame.display.update()
     for event in pygame.event.get():
         # Check to see if the "Close" button has been pressed
