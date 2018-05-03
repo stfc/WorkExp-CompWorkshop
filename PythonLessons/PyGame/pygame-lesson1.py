4a51fb519d76385a5e527b9b5d781cdcd7521275
Greg Corbett <greg.corbett@stfc.ac.uk>
2018-05-03 10:35:15 +0100

    Ensure a space after every comma

--- PythonLessons/PyGame/pygame-lesson1.py
+++ PythonLessons/PyGame/pygame-lesson1.py
@@ -22,9 +22,9 @@ while True:
     # draw the text onto the surface
     WINDOW_SURFACE.blit(TEXT, textRect)
 
-    pygame.draw.circle(WINDOW_SURFACE, (255,0,0), (10, 20), 30)
-    pygame.draw.line(WINDOW_SURFACE, (0,0,255), (200, 250), (225, 300))
-    pygame.draw.rect(WINDOW_SURFACE, (0,255,0), (150,50,100,50))
+    pygame.draw.circle(WINDOW_SURFACE, (255, 0, 0), (10, 20), 30)
+    pygame.draw.line(WINDOW_SURFACE, (0, 0, 255), (200, 250), (225, 300))
+    pygame.draw.rect(WINDOW_SURFACE, (0, 255, 0), (150, 50, 100, 50))
 
     pygame.display.update()
     for event in pygame.event.get():
