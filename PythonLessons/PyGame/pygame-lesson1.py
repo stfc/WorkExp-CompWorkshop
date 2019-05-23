1dfa68286359f7c015fc9f78385fce2981c14f61
Greg Corbett <greg.corbett@stfc.ac.uk>
2019-05-23 11:15:23 +0100

    Move "game logic" to for loop checking user input
    - the example on page 329 of the book uses this layout,
      so let's follow it.

--- PythonLessons/PyGame/pygame-lesson1.py
+++ PythonLessons/PyGame/pygame-lesson1.py
@@ -19,28 +19,29 @@ WINDOW_SURFACE = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
 BASIC_FONT = pygame.font.SysFont(None, 48)
 TEXT = BASIC_FONT.render('Hello world!', True, WHITE, BLACK)
 
+
 while True:
-    # Fill the screen to prevent any moving shapes leaving a 'trail'
-    WINDOW_SURFACE.fill(BLACK)
-
-    # Determine where to display TEXT
-    text_rect = TEXT.get_rect()
-    text_rect.centerx = WINDOW_SURFACE.get_rect().centerx
-    text_rect.centery = WINDOW_SURFACE.get_rect().centery
-    # Draw the text onto the surface
-    WINDOW_SURFACE.blit(TEXT, text_rect)
-
-    # Draw a circle, line and rectangle just to show how
-    pygame.draw.circle(WINDOW_SURFACE, (255, 0, 0), (10, 20), 30)
-    pygame.draw.line(WINDOW_SURFACE, (0, 0, 255), (200, 250), (225, 300))
-    pygame.draw.rect(WINDOW_SURFACE, (0, 255, 0), (150, 50, 100, 50))
-
-    # Update the screen
-    pygame.display.update()
-
     # Check for user input
     for event in pygame.event.get():
         # Check to see if the "Close" button has been pressed
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit(0)
+
+        # Fill the screen to prevent any moving shapes leaving a 'trail'
+        WINDOW_SURFACE.fill(BLACK)
+
+        # Determine where to display TEXT
+        text_rect = TEXT.get_rect()
+        text_rect.centerx = WINDOW_SURFACE.get_rect().centerx
+        text_rect.centery = WINDOW_SURFACE.get_rect().centery
+        # Draw the text onto the surface
+        WINDOW_SURFACE.blit(TEXT, text_rect)
+
+        # Draw a circle, line and rectangle just to show how
+        pygame.draw.circle(WINDOW_SURFACE, (255, 0, 0), (10, 20), 30)
+        pygame.draw.line(WINDOW_SURFACE, (0, 0, 255), (200, 250), (225, 300))
+        pygame.draw.rect(WINDOW_SURFACE, (0, 255, 0), (150, 50, 100, 50))
+
+        # Update the screen
+        pygame.display.update()
